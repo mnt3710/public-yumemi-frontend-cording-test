a1e9bcd3635c6f02e518e86d0370c434c05290f0
mnt3710 <tomato3710@gmail.com>
2022-10-24 05:58:06 +0900

    headerにflex追加

--- components/index/organisms/Header.vue
+++ components/index/organisms/Header.vue
@@ -12,3 +12,13 @@ export default Vue.extend({
   components: { HeaderTitle },
 })
 </script>
+
+<style scoped>
+.index-organisms-header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5vh;
+  background-color: #3856ff8f;
+}
+</style>
